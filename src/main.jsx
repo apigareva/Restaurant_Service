f98a11b72ce7cafb655a87145660489ef6911109
Aleksandra Druzhkova <druzhkova.aleksandra@gmail.com>
2025-10-08 15:26:50 +0300

    create restaurant list

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import { restaurants } from './restaurants_list.js'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+function menuRender(menu) {
+  const dishesList = menu.map((dish) => 
+    <div key={dish.id}>
+      <li>{dish.name} - {dish.price}$</li>
+    </div>
+  )
+  return <ul>{dishesList}</ul>
+}
+
+function reviewsRender(reviews){
+  const reviewsList = reviews.map(review =>
+    <div key={review.id}>
+      <li>{review.user}: "{review.text}"</li>
+    </div>
+  )
+  return <ul>{reviewsList}</ul>
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    restaurants.map(restaurant => 
+        <div key={restaurant.id}>
+          <h2>{restaurant.name}</h2>
+          <h3>Menu</h3>
+          {menuRender(restaurant.menu)}
+          <h3>Reviews</h3>
+          {reviewsRender(restaurant.reviews)}
+        </div>
+    )
 )
