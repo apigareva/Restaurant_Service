54db1e0209d54735b8e836d6fe5c43c9867a9ec9
Aleksandra Druzhkova <druzhkova.aleksandra@gmail.com>
2025-10-27 00:58:34 +0300

    remove dishCounter css, add useCallback to dishCounter

--- src/components/dishCounter/dishCounter.jsx
+++ src/components/dishCounter/dishCounter.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { Counter } from "../counter/counter"
-import styles from './dishCounter.module.css'
 
 const MIN_DISH_COUNTER = 0;
 const MAX_DISH_COUNTER = 5;
@@ -8,15 +7,16 @@ const MAX_DISH_COUNTER = 5;
 export const DishCounter = () => {
     const [value, setValue] = useState(0);
 
-    const increment = () => setValue(Math.min((value + 1), MAX_DISH_COUNTER));
-    const decrement = () => setValue(Math.max((value - 1), MIN_DISH_COUNTER));
+    const increment = useCallback(() => 
+        setValue(prevValue => Math.min((prevValue + 1), MAX_DISH_COUNTER)), []);
+    const decrement = useCallback(() => 
+        setValue(prevValue => Math.max((prevValue - 1), MIN_DISH_COUNTER)), []);
 
     return (
         <Counter 
             value={value} 
             increment={increment} 
             decrement={decrement} 
-            className={styles.root}
         />
     )
 }
