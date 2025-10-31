bc827d4be009489affe8b6e653d8ddd9725d67c6
Aleksandra Druzhkova <druzhkova.aleksandra@gmail.com>
2025-10-31 14:40:24 +0300

    fixed title in home container

--- src/components/homeContainer/homeContainer.jsx
+++ src/components/homeContainer/homeContainer.jsx
@@ -4,7 +4,7 @@ import { TabLink } from '../tabLink/tabLink';
 export const HomeContainer = () => {
     return (
         <div className={styles.root}>
-            <span>Welcome to restaunts delivery service!</span>
+            <span>Welcome to restaurants delivery service!</span>
             <TabLink title={'Go to choose restaurant'} pathname={'restaurants'} size={500} className={styles.link}/>
         </div>
     )
