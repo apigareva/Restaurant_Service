58c0aac80b2c3aca4eec869e22890819a689a90e
Aleksandra Druzhkova <druzhkova.aleksandra@gmail.com>
2025-12-08 11:26:09 +0300

    remove entitites

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { restaurantsSlice } from "./entities/restaurants/slice";
-import { dishesSlice } from "./entities/dishes/slice";
-import { reviewsSlice } from "./entities/reviews/slice";
-import { usersSlice } from "./entities/users/slice";
 import { cartSlice } from "./entities/cart/slice";
 import { apiSlice } from "./services/api.js";
 
 export const store = configureStore({
     reducer: {
-/*        [restaurantsSlice.name]: restaurantsSlice.reducer,
-        [dishesSlice.name]: dishesSlice.reducer,
-        [reviewsSlice.name]: reviewsSlice.reducer,
-        [usersSlice.name]: usersSlice.reducer,*/
         [cartSlice.name]: cartSlice.reducer,
         [apiSlice.reducerPath]: apiSlice.reducer
     },
